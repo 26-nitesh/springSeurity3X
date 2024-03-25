8b3c90b69362fe2aec8e5904c4b4a2e9c00056df
nkumar <nkumar@4321#>
2024-03-25 23:54:23 +0530

    't'

--- ems1/src/main/java/com/ems/repo/EmpRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ems.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ems.entity.Employee;
-
-public interface EmpRepo extends JpaRepository<Employee, Integer>{
-
-}
